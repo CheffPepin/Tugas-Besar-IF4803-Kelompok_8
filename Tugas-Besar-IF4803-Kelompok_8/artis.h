072cd4a560ecd365cae51b42f1b51b1c5e2e3712
CheffPepin <callistaazizahputri@gmail.com>
2025-12-05 12:54:17 +0700

    Update artis.h

---
+++ Tugas-Besar-IF4803-Kelompok_8/artis.h
@@ -0,0 +1,27 @@
+#ifndef ARTIS_H_INCLUDED
+#define ARTIS_H_INCLUDED
+#include <iostream>
+#include "string"
+using namespace std;
+
+typedef struct elmArtis *adrArtis;
+
+struct artis{
+    string nama;
+    float rating;
+};
+
+struct elmArtis{
+    artis info;
+    adrArtis next;
+};
+
+struct listArtis{
+    adrArtis first;
+};
+
+
+
+
+
+#endif // ARTIS_H_INCLUDED
