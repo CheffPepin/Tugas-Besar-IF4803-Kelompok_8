ad393ba1373a5f816077f187856532aa73758f21
MANG <moch.azminajibgunawan@gmail.com>
2025-12-13 21:23:09 +0700

    Implement menu loop for Admin and User selection
    Added a menu loop for user interaction with options for Admin and User.

--- Tugas-Besar-IF4803-Kelompok_8/main.cpp
+++ Tugas-Besar-IF4803-Kelompok_8/main.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
+#include "artis.h"
+#include "film.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int x;
+    while (x != 0){
+        cout << "===\n";
+        cout << "Menu";
+        cout << "===\n";
+        cout << "1. Admin\n";
+        cout << "2. User\n";
+        cout << "0. Kembali\n";
+        cout << "===\n";
+        cout << "Pilih :";
+        if (x == 1){
+            menuAdmin();
+            system(clear);
+        } else if (x == 2){
+            menuUser();
+            system(clear);
+        } else if (x < 0 && x > 2){
+            cout << "===\n";
+            cout << "Inputan tidak sesuai yang diberikan\n";
+            cout << "===\n";
+        } else {
+            cout << "===\n";
+            cout << "Terimakasih telah menggunakan program ini\n";
+        }
+    }
     return 0;
 }
