cb1387c95dd6d40ae0af80eb36d66969554647d7
MANG <moch.azminajibgunawan@gmail.com>
2025-12-13 21:49:07 +0700

    Refactor createElementChild for clarity

--- Tugas-Besar-IF4803-Kelompok_8/artis_103012400090.cpp
+++ Tugas-Besar-IF4803-Kelompok_8/artis_103012400090.cpp
@@ -1,17 +1,18 @@
-#include "film.h"
 #include <iostream>
-#include "string"
 #include "artis.h"
 using namespace std;
 
 adrArtis createElementChild(string nama, int umur, float rating){
-    adrArtis p = new elmArtis;
+    adrArtis p;
+    p = new elmArtis;
     p->info.nama = nama;
     p->info.umur = umur;
     p->info.rating = rating;
     p->next = nullptr;
+
     return p;
 }
+
 void insertFirstChild(adrFilm &q, adrArtis p){
         p->next = q->cast;
         q->cast = p;
@@ -32,5 +33,4 @@ void insertAfterChild(adrFilm &q, adrArtis prec, adrArtis p){
     if (prec != nullptr)
         p->next = prec->next;
         prec->next = p;
-    }
 }
