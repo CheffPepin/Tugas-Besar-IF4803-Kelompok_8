5710369accd2f818e17f2e7f5885189d723fb724
MANG <moch.azminajibgunawan@gmail.com>
2025-12-13 21:53:25 +0700

    Add list operations to film.h
    Added function declarations for list operations.

--- Tugas-Besar-IF4803-Kelompok_8/film.h
+++ Tugas-Besar-IF4803-Kelompok_8/film.h
@@ -6,6 +6,7 @@
 using namespace std;
 
 typedef struct elmFilm *adrFilm;
+typedef struct elmArtis *adrArtis;
 
 struct film{
     string nama;
@@ -22,5 +23,15 @@ struct elmFilm{
 struct listFilm{
     adrFilm first, last;
 };
+void createListParent(listFilm &L);
+adrFilm createElementParent(listFilm &L, string nama, int tahun, float rate);
+void insertFirstParent(listFilm &L, adrFilm p);
+void insertLastParent(listFilm &L, adrFilm p);
+void insertAfterParent(listFilm &L, adrFilm p, adrFilm prev);
+void deleteFirstParent(listFilm &L, adrFilm p);
+void deleteLastParent(listFilm &L, adrFilm p);
+void deleteAfterParent(listFilm &L, adrFilm prec, adrFilm p);
+adrFilm findElemenParent(listFilm L, string name);
+void viewParent(listFilm L);
 
 #endif // FILM_H_INCLUDED
