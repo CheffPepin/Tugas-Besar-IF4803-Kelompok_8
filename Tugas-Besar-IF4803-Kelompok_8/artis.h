0d9f416b6f20f6ffa36c4d30747951bc6b4141ed
MANG <moch.azminajibgunawan@gmail.com>
2025-12-13 21:05:00 +0700

    Add film.h and new functions to artis.h

--- Tugas-Besar-IF4803-Kelompok_8/artis.h
+++ Tugas-Besar-IF4803-Kelompok_8/artis.h
@@ -2,9 +2,11 @@
 #define ARTIS_H_INCLUDED
 #include <iostream>
 #include "string"
+#include "film.h"
 using namespace std;
 
 typedef struct elmArtis *adrArtis;
+typedef struct elmFilm *adrFilm;
 
 struct artis{
     string nama;
@@ -17,11 +19,20 @@ struct elmArtis{
     adrArtis next;
 };
 
-struct listArtis{
-    adrArtis first;
-}
+
+adrArtis createElementChild(string nama, int umur, float rating);
+void insertFirstChild(adrFilm &q, adrArtis p);
+void insertLastChild(adrFilm &q, adrArtis p);
+void insertAfterChild(adrFilm &q, adrArtis prec, adrArtis p);
+void deleteFirstChild(adrFilm &q, adrArtis p);
+void deleteLastChild(adrFilm &q, adrArtis p){;
+void deleteAfterChild(adrFilm &q, adrArtis p, adrArtis prec);
+adrArtis findElemenChild(adrFilm p, string nama);
+void viewChild(listFilm L1, listArtis L2);
 
 #endif // ARTIS_H_INCLUDED
+
+
 
 
 
