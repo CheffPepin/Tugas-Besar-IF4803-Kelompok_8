e147611e6fcb31fbb9dfcebf1b8036872fa77985
MANG <moch.azminajibgunawan@gmail.com>
2025-12-13 21:54:32 +0700

    Add admin menu functionality in main_admin.cpp
    Implement admin menu for film and actor management.

---
+++ Tugas-Besar-IF4803-Kelompok_8/main_admin.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "artis.h"
+#include "film.h"
+
+using namespace std;
+
+void menuAdmin(){
+    int x = -99
+    while (x != 0){
+        cout << "===\n";
+        cout << "Menu Admin";
+        cout << "===\n";
+        cout << "1. Insert First Film\n";
+        cout << "2. Insert Last Film\n";
+        cout << "3. Insert After Film\n";
+        cout << "4. Delete First Film\n";
+        cout << "5. Delete Last Film\n";
+        cout << "6. Delete After Film\n";
+        cout << "7. Find Nama Film\n";
+        cout << "8. View Film\n";
+        cout << "===\n";
+        cout << "9. Insert First Aktor\n";
+        cout << "10. Insert Last Aktor\n";
+        cout << "11. Insert After Aktor\n";
+        cout << "12. Delete First Aktor\n";
+        cout << "13. Delete Last Aktor\n";
+        cout << "14. Delete After Aktor\n";
+        cout << "15. Find Nama Aktor\n";
+        cout << "16. View Aktor\n";
+        cout << "0. Kembali\n";
+        cout << "===\n";
+        cout << "Pilih :";
+        if (x == 1){
+
+        }
+}
