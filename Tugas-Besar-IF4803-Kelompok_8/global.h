6661f2022dae0c8fd8b9c652fac24d4d2dcd2d7b
CheffPepin <callistaazizahputri@gmail.com>
2025-12-14 14:18:49 +0700

    Update and rename relasi.h to global.h

---
+++ Tugas-Besar-IF4803-Kelompok_8/global.h
@@ -0,0 +1,16 @@
+// global.h
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+#include "film.h"
+#include "artis.h"
+#include <string>
+
+extern listFilm L;
+extern adrFilm p;
+extern adrArtis q;
+extern string nama;
+extern int tahun;
+extern float rating;
+
+#endif
