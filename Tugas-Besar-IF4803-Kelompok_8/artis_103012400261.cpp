638a0b2d3748ff772200a75954da9cfd6a856236
CheffPepin <callistaazizahputri@gmail.com>
2025-12-14 11:33:21 +0700

    Update artis_103012400261.cpp

--- Tugas-Besar-IF4803-Kelompok_8/artis_103012400261.cpp
+++ Tugas-Besar-IF4803-Kelompok_8/artis_103012400261.cpp
@@ -1,7 +1,6 @@
 #include "film.h"
 #include <iostream>
 #include "string"
-#include "artis.h"
 using namespace std;
 
 void deleteFirstChild(adrFilm &q, adrArtis p){
@@ -9,7 +8,7 @@ void deleteFirstChild(adrFilm &q, adrArtis p){
      p = nullptr;
     } else if(q ->cast ->next == nullptr){
         p = q ->cast;
-        q ->cast == nullptr;
+        q ->cast = nullptr;
     } else {
         p = q ->cast;
         q ->cast = q ->cast ->next;
@@ -24,7 +23,7 @@ void deleteLastChild(adrFilm &q, adrArtis p){
     if(x== nullptr){
 
     } else if(x->next == nullptr){
-         x == nullptr;
+         x = nullptr;
     } else{
         while(x ->next ->next != nullptr){
             x = x ->next;
@@ -48,25 +47,58 @@ void deleteAfterChild(adrFilm &q, adrArtis p, adrArtis prec){
 
 adrArtis findElemenChild(adrFilm p, string nama){
     adrArtis q = p ->cast;
-    while(q ->info.nama != nama && q != nullptr){
+    while( q != nullptr && q ->info.nama != nama){
         q = q ->next;
     }
     return q;
 }
 
-void viewChild(adrFilm p){
-    adrArtis q = p ->cast;
+void viewChild(listFilm L){
+   string Aktor[20];
+   int x,i;
 
-    while(p != nullptr){
-        cout << "Judul film : " << p ->info.nama << endl;
-        cout << "Daftar Pemain :" << endl;
-        while(q != nullptr){
-            cout << q ->info.nama << "( " << q ->info.umur << " )" << endl;
-            cout << "dengan rating : " << q ->info.rating << endl;
-            q = q ->next;
+   listAktor(L, Aktor,x);
+
+   for(i = 0; i < x; i++){
+        cout << "Nama Artis : " << Aktor[i] << endl;
+        cout << "berperan di film : ";
+        adrFilm p = L.first;
+        while(p != nullptr){
+            adrArtis q = p ->cast;
+            while(q != nullptr){
+                if(q ->info.nama == Aktor[i]){
+                    cout << p ->info.nama << ", ";
+                }
+                q = q ->next;
+            }
+            p = p ->next;
         }
-        p = p ->next;
-    }
+        cout << endl;
+   }
+
 }
 
 
+void listAktor(listFilm L, string Aktor[], int &x){
+    adrFilm p = L.first;
+    adrArtis q;
+    x = 0;
+
+    while(p != nullptr){
+        q = p->cast;
+        while(q != nullptr){
+
+            int i = 0;
+            while(i < x && Aktor[i] != q->info.nama){
+                i++;
+            }
+            if(i == x){
+                Aktor[x] = q->info.nama;
+                x++;
+            }
+
+            q = q->next;
+        }
+        p = p->next;
+    }
+}
